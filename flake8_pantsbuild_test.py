37d3c91a827b82c8214e0d29a4f456ee3bc50f52
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2020-03-15 09:24:13 -0700

    Reshuffle error code numbers (#15)
    Flake8 only requires that codes have at least 3 characters, so we could use codes like `PB1`, `PB2`, etc. We go with two-digit numbers to allow more lints than 10. This is much easier to reason with than 3-digits, e.g. `PB20 vs. PB200`.
    
    We use this scheme:
    
    `1*` -> logical issues
    `20` -> 2-space indentation
    `30` -> trailing slashes
    `6*` -> Py2 to Py3 lints (2 x 3 = 6)

--- flake8_pantsbuild_test.py
+++ flake8_pantsbuild_test.py
@@ -10,19 +10,19 @@ from textwrap import dedent
 import pytest
 
 from flake8_pantsbuild import (
-    PB100,
-    PB201,
-    PB601,
-    PB602,
-    PB603,
-    PB604,
-    PB605,
-    PB606,
-    PB607,
-    PB800,
-    PB802,
-    PB804,
-    PB805,
+    PB10,
+    PB11,
+    PB12,
+    PB13,
+    PB20,
+    PB30,
+    PB60,
+    PB61,
+    PB62,
+    PB63,
+    PB64,
+    PB65,
+    PB66,
     PY2,
 )
 
@@ -32,7 +32,70 @@ from flake8_pantsbuild import (
 # a set in every test for deduplication. See https://pypi.org/project/pytest-flake8dir/.
 
 
-def test_pb_100(flake8dir):
+def test_pb_10(flake8dir):
+    template = dedent(
+        """\
+        import os.path
+
+
+        class Example(object):
+            CONSTANT = "foo"
+
+            def foo(self, value):
+                return os.path.join({}.CONSTANT, value)
+        """
+    )
+    flake8dir.make_py_files(good=template.format("self"), bad=template.format("Example"))
+    result = flake8dir.run_flake8()
+    assert {"./bad.py:8:29: {}".format(PB10.format(name="Example", attr="CONSTANT"))} == set(
+        result.out_lines
+    )
+
+
+def test_pb_11(flake8dir):
+    violating_pairs = itertools.product([None, False, True, 1, "'a'"], ["or", "and"])
+    violations = {
+        "bad{}".format(i): "x = 0\n{constant} {op} x".format(constant=pair[0], op=pair[1])
+        for i, pair in enumerate(violating_pairs)
+    }
+    flake8dir.make_py_files(good="x = y = 0\nx or y", **violations)
+    result = flake8dir.run_flake8()
+    assert {"./{}.py:2:1: {}".format(fp, PB11) for fp in violations} == set(result.out_lines)
+
+
+def test_pb_12(flake8dir):
+    violations = {
+        "bad{}".format(i): "x = 0\nx and {}".format(constant)
+        for i, constant in enumerate([None, False, True, 1, "'a'"])
+    }
+    flake8dir.make_py_files(good="x = y = 0\nx and y", **violations)
+    result = flake8dir.run_flake8()
+    assert {"./{}.py:2:7: {}".format(fp, PB12) for fp in violations} == set(result.out_lines)
+
+
+def test_pb_13(flake8dir):
+    flake8dir.make_example_py(
+        dedent(
+            """\
+            foo = open('test.txt')
+
+            with open('test.txt'):
+                pass
+
+            bar = open('test.txt')
+
+            with open('test.txt') as fp:
+                fp.read()
+            """
+        )
+    )
+    result = flake8dir.run_flake8()
+    assert {"./example.py:1:7: {}".format(PB13), "./example.py:6:7: {}".format(PB13)} == set(
+        result.out_lines
+    )
+
+
+def test_pb_20(flake8dir):
     flake8dir.make_example_py(
         dedent(
             """\
@@ -57,15 +120,15 @@ def test_pb_100(flake8dir):
         )
     )
     result = flake8dir.run_flake8(
-        extra_args=["--enable-extensions", "PB100", "--extend-ignore", "E111"]
+        extra_args=["--enable-extensions", "PB20", "--extend-ignore", "E111"]
     )
     assert {
-        "./example.py:2:1: {}".format(PB100.format("1")),
-        "./example.py:6:1: {}".format(PB100.format("4")),
+        "./example.py:2:1: {}".format(PB20.format("1")),
+        "./example.py:6:1: {}".format(PB20.format("4")),
     } == set(result.out_lines)
 
 
-def test_pb_201(flake8dir):
+def test_pb_30(flake8dir):
     flake8dir.make_example_py(
         dedent(
             """\
@@ -93,14 +156,43 @@ def test_pb_201(flake8dir):
             """
         )
     )
-    result = flake8dir.run_flake8(extra_args=["--enable-extensions", "PB201"])
-    assert {"./example.py:3:7: {}".format(PB201), "./example.py:6:20: {}".format(PB201)} == set(
+    result = flake8dir.run_flake8(extra_args=["--enable-extensions", "PB30"])
+    assert {"./example.py:3:7: {}".format(PB30), "./example.py:6:20: {}".format(PB30)} == set(
+        result.out_lines
+    )
+
+
+@pytest.mark.skipif(not PY2, reason="Print statements cause syntax errors with Python 3")
+def test_pb_60(flake8dir):
+    flake8dir.make_py_files(
+        normal=dedent(
+            """\
+            # Good
+            print("I'm a statement, but look like a function call")
+            print(0, 1, 2)
+            print (0, 1, 2)
+
+            # Bad
+            print "old school"
+            print 0
+            """
+        ),
+        future=dedent(
+            """\
+            from __future__ import print_function
+
+            print("Future-proof!")
+            """
+        ),
+    )
+    result = flake8dir.run_flake8(extra_args=["--enable-extensions", "PB6"])
+    assert {"./normal.py:7:1: {}".format(PB60), "./normal.py:8:1: {}".format(PB60)} == set(
         result.out_lines
     )
 
 
 @pytest.mark.skipif(not PY2, reason="Old-style exceptions cause syntax error with Python 3")
-def test_pb_601(flake8dir):
+def test_pb_61(flake8dir):
     flake8dir.make_example_py(
         dedent(
             """\
@@ -132,12 +224,12 @@ def test_pb_601(flake8dir):
         )
     )
     result = flake8dir.run_flake8(extra_args=["--enable-extensions", "PB6"])
-    assert {"./example.py:3:1: {}".format(PB601), "./example.py:8:1: {}".format(PB601)} == set(
+    assert {"./example.py:3:1: {}".format(PB61), "./example.py:8:1: {}".format(PB61)} == set(
         result.out_lines
     )
 
 
-def test_pb_602(flake8dir):
+def test_pb_62(flake8dir):
     flake8dir.make_example_py(
         dedent(
             """\
@@ -167,15 +259,15 @@ def test_pb_602(flake8dir):
     )
     result = flake8dir.run_flake8(extra_args=["--enable-extensions", "PB6"])
     assert {
-        "./example.py:7:1: {}".format(PB602.format(bad_attr="iteritems", good_attr="items")),
-        "./example.py:8:1: {}".format(PB602.format(bad_attr="iterkeys", good_attr="keys")),
-        "./example.py:9:1: {}".format(PB602.format(bad_attr="itervalues", good_attr="values")),
-        "./example.py:21:1: {}".format(PB602.format(bad_attr="iteritems", good_attr="items")),
+        "./example.py:7:1: {}".format(PB62.format(bad_attr="iteritems", good_attr="items")),
+        "./example.py:8:1: {}".format(PB62.format(bad_attr="iterkeys", good_attr="keys")),
+        "./example.py:9:1: {}".format(PB62.format(bad_attr="itervalues", good_attr="values")),
+        "./example.py:21:1: {}".format(PB62.format(bad_attr="iteritems", good_attr="items")),
     } == set(result.out_lines)
 
 
 @pytest.mark.skipif(not PY2, reason="`xrange()` does not exist in Python 3`")
-def test_pb_603(flake8dir):
+def test_pb_63(flake8dir):
     flake8dir.make_example_py(
         dedent(
             """\
@@ -193,11 +285,11 @@ def test_pb_603(flake8dir):
         )
     )
     result = flake8dir.run_flake8(extra_args=["--enable-extensions", "PB6"])
-    assert {"./example.py:4:1: {}".format(PB603)} == set(result.out_lines)
+    assert {"./example.py:4:1: {}".format(PB63)} == set(result.out_lines)
 
 
 @pytest.mark.skipif(not PY2, reason="`basestring` and `unicode` do not exist in Python 3`")
-def test_pb_604(flake8dir):
+def test_pb_64(flake8dir):
     flake8dir.make_example_py(
         dedent(
             """\
@@ -209,15 +301,15 @@ def test_pb_604(flake8dir):
     result = flake8dir.run_flake8(extra_args=["--enable-extensions", "PB6"])
     assert {
         "./example.py:1:26: {}".format(
-            PB604.format(bad_name="unicode", six_replacement="text_type")
+            PB64.format(bad_name="unicode", six_replacement="text_type")
         ),
         "./example.py:2:26: {}".format(
-            PB604.format(bad_name="basestring", six_replacement="string_types")
+            PB64.format(bad_name="basestring", six_replacement="string_types")
         ),
     } == set(result.out_lines)
 
 
-def test_pb_605(flake8dir):
+def test_pb_65(flake8dir):
     flake8dir.make_example_py(
         dedent(
             """\
@@ -240,10 +332,10 @@ def test_pb_605(flake8dir):
         )
     )
     result = flake8dir.run_flake8(extra_args=["--enable-extensions", "PB6"])
-    assert {"./example.py:6:5: {}".format(PB605)} == set(result.out_lines)
+    assert {"./example.py:6:5: {}".format(PB65)} == set(result.out_lines)
 
 
-def test_pb_606(flake8dir):
+def test_pb_66(flake8dir):
     flake8dir.make_example_py(
         dedent(
             """\
@@ -268,98 +360,6 @@ def test_pb_606(flake8dir):
         )
     )
     result = flake8dir.run_flake8(extra_args=["--enable-extensions", "PB6"])
-    assert {"./example.py:4:1: {}".format(PB606), "./example.py:16:1: {}".format(PB606)} == set(
+    assert {"./example.py:4:1: {}".format(PB66), "./example.py:16:1: {}".format(PB66)} == set(
         result.out_lines
     )
-
-
-@pytest.mark.skipif(not PY2, reason="Print statements cause syntax errors with Python 3")
-def test_pb_607(flake8dir):
-    flake8dir.make_py_files(
-        normal=dedent(
-            """\
-            # Good
-            print("I'm a statement, but look like a function call")
-            print(0, 1, 2)
-            print (0, 1, 2)
-
-            # Bad
-            print "old school"
-            print 0
-            """
-        ),
-        future=dedent(
-            """\
-            from __future__ import print_function
-
-            print("Future-proof!")
-            """
-        ),
-    )
-    result = flake8dir.run_flake8(extra_args=["--enable-extensions", "PB6"])
-    assert {"./normal.py:7:1: {}".format(PB607), "./normal.py:8:1: {}".format(PB607)} == set(
-        result.out_lines
-    )
-
-
-def test_pb_800(flake8dir):
-    template = dedent(
-        """\
-        import os.path
-
-
-        class Example(object):
-            CONSTANT = "foo"
-
-            def foo(self, value):
-                return os.path.join({}.CONSTANT, value)
-        """
-    )
-    flake8dir.make_py_files(good=template.format("self"), bad=template.format("Example"))
-    result = flake8dir.run_flake8()
-    assert {"./bad.py:8:29: {}".format(PB800.format(name="Example", attr="CONSTANT"))} == set(
-        result.out_lines
-    )
-
-
-def test_pb_802(flake8dir):
-    flake8dir.make_example_py(
-        dedent(
-            """\
-            foo = open('test.txt')
-
-            with open('test.txt'):
-                pass
-
-            bar = open('test.txt')
-
-            with open('test.txt') as fp:
-                fp.read()
-            """
-        )
-    )
-    result = flake8dir.run_flake8()
-    assert {"./example.py:1:7: {}".format(PB802), "./example.py:6:7: {}".format(PB802)} == set(
-        result.out_lines
-    )
-
-
-def test_pb_804(flake8dir):
-    violating_pairs = itertools.product([None, False, True, 1, "'a'"], ["or", "and"])
-    violations = {
-        "bad{}".format(i): "x = 0\n{constant} {op} x".format(constant=pair[0], op=pair[1])
-        for i, pair in enumerate(violating_pairs)
-    }
-    flake8dir.make_py_files(good="x = y = 0\nx or y", **violations)
-    result = flake8dir.run_flake8()
-    assert {"./{}.py:2:1: {}".format(fp, PB804) for fp in violations} == set(result.out_lines)
-
-
-def test_pb_805(flake8dir):
-    violations = {
-        "bad{}".format(i): "x = 0\nx and {}".format(constant)
-        for i, constant in enumerate([None, False, True, 1, "'a'"])
-    }
-    flake8dir.make_py_files(good="x = y = 0\nx and y", **violations)
-    result = flake8dir.run_flake8()
-    assert {"./{}.py:2:7: {}".format(fp, PB805) for fp in violations} == set(result.out_lines)
